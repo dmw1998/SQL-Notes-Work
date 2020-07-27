8b9b5aa0a5c2a357b474ded8c175e12b54c13ba1
dmw1998 <dmw1998@users.noreply.github.com>
2020-07-27 22:39:47 +0800

    Update import data.sql

--- import data.sql
+++ import data.sql
@@ -1,19 +1,22 @@
+select distinct userid, amount from order 
+group by userid order by paytime limit 1;
+
 use world;
 CREATE TABLE `NYAB` (
-  `id` int(11) default null,
+  `id` integer default null,
   `name` varchar(255) DEFAULT NULL,
-  `host_id` int(11) DEFAULT NULL,
+  `host_id` integer DEFAULT NULL,
   `host_name` varchar(255) DEFAULT NULL,
   `neighbourhood_group` varchar(255) DEFAULT NULL,
   `neighbourhood` varchar(255) DEFAULT NULL,
   `latitude` double default null,
   `longitude` double default null,
   `room_type` varchar(255) DEFAULT NULL,
-  `price` int(11) default null,
-  `minimum_nights` int(11) default null,
-  `number_of_reviews` int(11) default null,
+  `price` integer default null,
+  `minimum_nights` integer default null,
+  `number_of_reviews` integer default null,
   `last_review` date default null,
   `reviews_per_month` double default null,
-  `calculated_host_listings_count` int(11) default null,
-  `availability_365` int(11) default null
+  `calculated_host_listings_count` integer default null,
+  `availability_365` integer default null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
